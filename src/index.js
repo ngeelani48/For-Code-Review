0c736012eb1266553b81dcdc557bddbd69b74ad5
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-02-27 17:49:33 +0300

    Commit for code review

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import './style.css';
+import {
+  FnToDoList,
+  addTask,
+  editTask,
+  deleteTask,
+} from './modules/UI.js';
+import clearCompleted from './modules/clearCompleted.js';
+import markTask from './modules/marktask.js';
+import { updateLocalStorage, getLocalStorage } from './modules/storage.js';
+
+const input = document.querySelector('.input');
+const List = document.querySelector('.list-ul');
+const clearCompletedBtn = document.querySelector('.clear-btn');
+const addTaskBtn = document.querySelector('.add-btn');
+
+let Tasks = getLocalStorage();
+
+FnToDoList(Tasks);
+
+input.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter' && input.value !== '') {
+    addTask(Tasks, input.value);
+    input.value = '';
+    updateLocalStorage(Tasks);
+    FnToDoList(Tasks);
+  }
+});
+
+addTaskBtn.addEventListener('click', () => {
+  if (input.value !== '') {
+    addTask(Tasks, input.value);
+    input.value = '';
+    updateLocalStorage(Tasks);
+    FnToDoList(Tasks);
+  }
+});
+
+List.addEventListener('click', (e) => {
+  if (e.target.closest('.list-li-checkbox')) {
+    markTask(e, Tasks);
+  }
+  updateLocalStorage(Tasks);
+  FnToDoList(Tasks);
+});
+
+clearCompletedBtn.addEventListener('click', () => {
+  Tasks = clearCompleted(Tasks);
+  updateLocalStorage(Tasks);
+  FnToDoList(Tasks);
+});
+
+List.addEventListener('dblclick', (e) => {
+  if (e.target.closest('.list-li-text')) {
+    editTask(e, Tasks);
+  }
+});
+
+List.addEventListener('click', (e) => {
+  if (e.target.closest('.cross-sign')) {
+    deleteTask(e, Tasks);
+  }
+});
---
+++ src/modules/UI.js
@@ -0,0 +1,63 @@
+import { updateLocalStorage, getLocalStorage } from './storage.js';
+
+const List = document.querySelector('.list-ul');
+
+export const FnToDoList = (ListArray) => {
+  List.innerHTML = '';
+
+  ListArray = getLocalStorage();
+
+  ListArray.forEach((toDo) => {
+    const toDoItem = document.createElement('li');
+    toDoItem.classList.add('list-li');
+    toDoItem.innerHTML = `
+    <input type="checkbox" class="list-li-checkbox" ${toDo.completed ? 'checked' : ''}>
+        `;
+
+    const toDoText = document.createElement('input');
+    toDoText.classList.add('list-li-text');
+    toDoText.value = toDo.task;
+    toDoText.disabled = true;
+    toDoItem.appendChild(toDoText);
+
+    const crossIcon = document.createElement('span');
+    crossIcon.classList.add('cross-sign');
+    crossIcon.innerHTML = 'x';
+    toDoItem.appendChild(crossIcon);
+
+    List.appendChild(toDoItem);
+  });
+};
+
+export const addTask = (ListArray, task) => {
+  ListArray.push({ task, completed: false, id: ListArray.length + 1 });
+};
+
+export const editTask = (e, ListArray) => {
+  const clickedTask = e.target.closest('.list-li-text');
+  clickedTask.disabled = false;
+  clickedTask.focus();
+  const taskText = clickedTask.value;
+
+  clickedTask.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter' && clickedTask.value !== '') {
+      const taskIndex = ListArray.findIndex((task) => task.task === taskText);
+      ListArray[taskIndex].task = clickedTask.value;
+      clickedTask.disabled = true;
+      updateLocalStorage(ListArray);
+      FnToDoList(ListArray);
+    }
+  });
+};
+
+export const deleteTask = (e, ListArray) => {
+  const clickedCross = e.target.closest('.cross-sign');
+  const clickedTask = clickedCross.previousElementSibling;
+  const taskIndex = ListArray.findIndex((task) => task.task === clickedTask.value);
+  ListArray.splice(taskIndex, 1);
+  ListArray.forEach((task, index) => {
+    task.id = index + 1;
+  });
+  updateLocalStorage(ListArray);
+  FnToDoList(ListArray);
+};
